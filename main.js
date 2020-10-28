951121254f06bdfadfa28291d85b7ce0b90bf41d
wenogk <drf325@nyu.edu>
2020-10-28 14:41:19 +0400

    finally able to get xpath

--- main.js
+++ main.js
@@ -60,7 +60,8 @@ function deInterleaveArray(arr) {
             backendNodeId: node.backendNodeId,
 
           }))
-        console.log(resolvedNode)
+        //  console.log(resolvedNode)
+        
         //
           try {
 
@@ -88,11 +89,46 @@ function deInterleaveArray(arr) {
             backendNodeId: node.backendNodeId,
           }))
 
+          let functionCode = `
+          function() {
+            elm = this;
+            var allNodes = document.getElementsByTagName('*'); 
+            for (var segs = []; elm && elm.nodeType == 1; elm = elm.parentNode) 
+            { 
+                if (elm.hasAttribute('id')) { 
+                        var uniqueIdCount = 0; 
+                        for (var n=0;n < allNodes.length;n++) { 
+                            if (allNodes[n].hasAttribute('id') && allNodes[n].id == elm.id) uniqueIdCount++; 
+                            if (uniqueIdCount > 1) break; 
+                        }; 
+                        if ( uniqueIdCount == 1) { 
+                            segs.unshift('id("' + elm.getAttribute('id') + '")'); 
+                            return segs.join('/'); 
+                        } else { 
+                            segs.unshift(elm.localName.toLowerCase() + '[@id="' + elm.getAttribute('id') + '"]'); 
+                        } 
+                } else if (elm.hasAttribute('class')) { 
+                    segs.unshift(elm.localName.toLowerCase() + '[@class="' + elm.getAttribute('class') + '"]'); 
+                } else { 
+                    for (i = 1, sib = elm.previousSibling; sib; sib = sib.previousSibling) { 
+                        if (sib.localName == elm.localName)  i++; }; 
+                        segs.unshift(elm.localName.toLowerCase() + '[' + i + ']'); 
+                }; 
+            }; 
+            return segs.length ? '/' + segs.join('/') : null;
+        }
+          `
+          //console.log("OBJECT ID " +(resolvedNode.object.objectId))
           nodeDescription.node.attributes = deInterleaveArray(nodeDescription.node.attributes)
           completeListenerObject.listeners[i]["indentifierInfo"] = nodeDescription.node
-
-        } catch {
-
+          let callXpathFunction = (await cdp.send('Runtime.callFunctionOn', {
+            functionDeclaration : functionCode,
+            objectId: resolvedNode.object.objectId,
+          })) 
+          
+          
+        } catch (e) {
+          //console.log(e)
             completeListenerObject.listeners[i]["indentifierInfo"] = null
             
         }
@@ -195,7 +231,7 @@ function deInterleaveArray(arr) {
 
       });
 
-      console.log(JSON.stringify(counterObj,null, 2))
+      //console.log(JSON.stringify(counterObj,null, 2))
       //console.log(JSON.stringify(completeListenerObject, null, 2));
     
 
